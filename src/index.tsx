1a55426ec3658816f13b1224da32cc4f534e3c58
GLDuval <glevesqueduval@gmail.com>
2023-03-21 15:18:07 -0400

    Update to typescript and update deps

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import * as React  from 'react';
+import NAV2D from './vendor/nav2d.js';
+import ROS2D from './vendor/ros2d.js';
+import ROSLIB from 'roslib';
+import PropTypes from 'prop-types';
+
+interface Nav2dProps {
+  ros?: ROSLIB.Ros;
+  id?: string;
+  width?: number;
+  height?: number;
+  serverName?: string;
+}
+
+const Nav2d: React.FC<Nav2dProps> = ({ ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' }), id = 'nav2d', width = 500, height = 500, serverName = '/move_base' }) => {
+  React.useEffect(() => {
+    const viewer = new ROS2D.Viewer({
+      divID: id,
+      width,
+      height,
+    });
+    const nav = NAV2D.OccupancyGridClientNav({
+      ros,
+      rootObject: viewer.scene,
+      viewer,
+      serverName,
+    });
+  }, [id, ros, serverName, height, width]);
+
+  return <div id={id} />;
+};
+
+export default Nav2d;
