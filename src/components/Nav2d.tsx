7ec5a3c00d9d4d0a31d54168119f48a10682903f
GLDuval <glevesqueduval@gmail.com>
2023-03-21 15:54:28 -0400

    Fix esm and cjs builds

--- src/components/Nav2d.tsx
+++ src/components/Nav2d.tsx
@@ -1,4 +1,4 @@
-import * as React  from 'react';
+import React, { FC, useEffect } from 'react';
 import NAV2D from '../vendor/nav2d.js';
 import ROS2D from '../vendor/ros2d.js';
 import ROSLIB from 'roslib';
@@ -11,8 +11,8 @@ interface Nav2dProps {
   serverName?: string;
 }
 
-const Nav2d: React.FC<Nav2dProps> = ({ ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' }), id = 'nav2d', width = 500, height = 500, serverName = '/move_base' }) => {
-  React.useEffect(() => {
+const Nav2d: FC<Nav2dProps> = ({ ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' }), id = 'nav2d', width = 500, height = 500, serverName = '/move_base' }) => {
+  useEffect(() => {
     const viewer = new ROS2D.Viewer({
       divID: id,
       width,
