8bf9ba946c031ea12f8f0120d86893791b0e505b
GLDuval <glevesqueduval@gmail.com>
2023-03-21 16:15:07 -0400

    Add vendor folder to build

---
+++ dist/cjs/vendor/ros2d.d.ts
@@ -0,0 +1,5 @@
+export default ROS2D;
+/**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+declare var ROS2D: any;
---
+++ dist/esm/vendor/nav2d.d.ts
@@ -0,0 +1,6 @@
+export default NAV2D;
+/**
+ * @author Russell Toris - rctoris@wpi.edu
+ * @author Lars Kunze - l.kunze@cs.bham.ac.uk
+ */
+declare var NAV2D: any;
